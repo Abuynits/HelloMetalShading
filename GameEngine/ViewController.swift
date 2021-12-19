6843a442d7daa05a5872915f09a646c01140c206
abuynits <abuynits@gmail.com>
2021-12-19 14:24:36 -0800

    Initial Commit

---
+++ GameEngine/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GameEngine
+//
+//  Created by Alexiy Buynitsky on 12/19/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
